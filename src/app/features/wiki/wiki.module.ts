ff1fbb5590fb31b57a223c532ee349d63e254dcb
InoaPSilva <inoa.silva@di2win.com>
2024-10-09 01:08:36 -0300

    build: created chat and wiki modules

---
+++ src/app/features/wiki/wiki.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WikiComponent } from './wiki.component';
+
+
+
+@NgModule({
+  declarations: [
+    WikiComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WikiModule { }
