64ec6adc921257f5cfeb4dbb7307818b14aa3352
Augusto <augusto.gomes@di2win.com>
2024-10-16 00:04:01 -0300

    Feat: deafault roots

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WikiComponent } from './features/wiki/wiki.component';
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
+import { FeaturesComponent } from './features/features.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    component: RegisterComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "home",
+    component: FeaturesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
+import { WikiComponent } from './features/wiki/wiki.component';
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { FeaturesComponent } from './features/features.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: RegisterComponent
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "home",
+        component: FeaturesComponent
+    },
+];
